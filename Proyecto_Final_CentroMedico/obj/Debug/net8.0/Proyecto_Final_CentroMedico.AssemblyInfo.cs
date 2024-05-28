1e34a199975e3934e2f2748473abc820af07e535
carlosquiros1995 <cquiros80363@ufide.ac.cr>
2024-05-28 12:12:51 -0600

    CM-1. Creación de la base de datos.
    En este commit se incluye el script de creación de la base de datos.

--- Proyecto_Final_CentroMedico/obj/Debug/net8.0/Proyecto_Final_CentroMedico.AssemblyInfo.cs
+++ Proyecto_Final_CentroMedico/obj/Debug/net8.0/Proyecto_Final_CentroMedico.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto_Final_CentroMedico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fe765e1ee140c53e3403f7c8b3a57db0f72b711")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto_Final_CentroMedico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto_Final_CentroMedico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
