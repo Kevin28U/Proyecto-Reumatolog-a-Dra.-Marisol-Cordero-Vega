011ece2d103083220dab3a20330de9a39aa9de8a
kevinga28 <kevinga2806@gmail.com>
2024-05-30 19:13:51 -0600

    Footer Responsive y fondo login

--- Proyecto_Final_CentroMedico/obj/Release/net8.0/Proyecto_Final_CentroMedico.AssemblyInfo.cs
+++ Proyecto_Final_CentroMedico/obj/Release/net8.0/Proyecto_Final_CentroMedico.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto_Final_CentroMedico")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd6af4e7c0cd016cb7af861a0fe38136e4599d00")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto_Final_CentroMedico")]
