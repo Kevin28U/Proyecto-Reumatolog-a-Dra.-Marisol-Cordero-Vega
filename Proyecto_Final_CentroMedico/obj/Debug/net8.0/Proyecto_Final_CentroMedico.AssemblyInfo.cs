a03122beaeb2f67ba157979666043ed4a76745df
KevinR22S <kramirez10960@ufide.ac.cr>
2024-05-30 20:23:00 -0600

    Cambios de diseño

--- Proyecto_Final_CentroMedico/obj/Debug/net8.0/Proyecto_Final_CentroMedico.AssemblyInfo.cs
+++ Proyecto_Final_CentroMedico/obj/Debug/net8.0/Proyecto_Final_CentroMedico.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto_Final_CentroMedico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd6af4e7c0cd016cb7af861a0fe38136e4599d00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7af3be5f66bc2c534d9410b8ac24cac77f82d6ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto_Final_CentroMedico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto_Final_CentroMedico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
