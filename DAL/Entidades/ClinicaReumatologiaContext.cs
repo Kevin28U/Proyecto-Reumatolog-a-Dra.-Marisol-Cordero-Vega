85ad381efa73d9c84580eb80f3168a90d7ea9372
FabianCR22 <cmorales00804@ufide.ac.cr>
2024-06-07 00:28:20 -0600

    Avance en CRUD de Roles
    Avance en CRUD de Roles // Listos Index y Crear

--- DAL/Entidades/ClinicaReumatologiaContext.cs
+++ DAL/Entidades/ClinicaReumatologiaContext.cs
@@ -43,8 +43,11 @@ public partial class ClinicaReumatologiaContext : DbContext
 
     public virtual DbSet<Usuario> Usuarios { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { 
-    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseSqlServer();
+
     }
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
        // => optionsBuilder.UseSqlServer("Server=MSI;Database=CLINICA_REUMATOLOGIA;Integrated Security=True;TrustServerCertificate=true");
--- Proyecto_Final_CentroMedico/Controllers/RolController.cs
+++ Proyecto_Final_CentroMedico/Controllers/RolController.cs
@@ -1,31 +1,156 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Proyecto_Final_CentroMedico;
 
 namespace Proyecto_Final_CentroMedico.Controllers
 {
-	public class RolController : Controller
-	{
-        public IActionResult Index()
-        {
-            ViewBag.ShowNavbar = true;
-            ViewBag.ShowFooter = false;
-            return View();
+    public class RolController : Controller
+    {
+        private readonly ClinicaReumatologiaContext _context;
 
+        public RolController(ClinicaReumatologiaContext context)
+        {
+            _context = context;
         }
 
+        // GET: Rol
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Rols.ToListAsync());
+        }
+
+        // GET: Rol/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rol = await _context.Rols
+                .FirstOrDefaultAsync(m => m.IdRol == id);
+            if (rol == null)
+            {
+                return NotFound();
+            }
+
+            return View(rol);
+        }
+
+        // GET: Rol/Create
         public IActionResult Crear()
         {
-            ViewBag.ShowNavbar = true;
-            ViewBag.ShowFooter = true;
             return View();
-
         }
 
-        public IActionResult Editar()
+        // POST: Rol/Crear
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Crear([Bind("IdRol,NombreRol")] Rol rol)
         {
-            ViewBag.ShowNavbar = true;
-            ViewBag.ShowFooter = true;
-            return View();
+            if (ModelState.IsValid)
+            {
+                _context.Add(rol);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(rol);
+        }
 
+        // GET: Rol/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rol = await _context.Rols.FindAsync(id);
+            if (rol == null)
+            {
+                return NotFound();
+            }
+            return View(rol);
+        }
+
+        // POST: Rol/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdRol,NombreRol")] Rol rol)
+        {
+            if (id != rol.IdRol)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(rol);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RolExists(rol.IdRol))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(rol);
+        }
+
+        // GET: Rol/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rol = await _context.Rols
+                .FirstOrDefaultAsync(m => m.IdRol == id);
+            if (rol == null)
+            {
+                return NotFound();
+            }
+
+            return View(rol);
+        }
+
+        // POST: Rol/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var rol = await _context.Rols.FindAsync(id);
+            if (rol != null)
+            {
+                _context.Rols.Remove(rol);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RolExists(int id)
+        {
+            return _context.Rols.Any(e => e.IdRol == id);
         }
     }
 }
