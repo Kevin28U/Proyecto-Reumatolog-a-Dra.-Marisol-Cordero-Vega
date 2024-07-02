df49d2bffbe848e922f89ba19fa47df93e9b436a
FabianCR22 <cmorales00804@ufide.ac.cr>
2024-07-02 17:48:10 -0600

    Se general controlador y vistas
    Se general controlador y vistas para la gestion de pagos. Tambien se le agrega diseño y validaciones.

---
+++ Proyecto_Final_CentroMedico/Controllers/PagosController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Proyecto_Final_CentroMedico;
+
+namespace Proyecto_Final_CentroMedico.Controllers
+{
+    public class PagosController : Controller
+    {
+        private readonly ClinicaReumatologiaContext _context;
+
+        public PagosController(ClinicaReumatologiaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Pagos
+        public async Task<IActionResult> Index()
+        {
+            var clinicaReumatologiaContext = _context.Pagos.Include(p => p.IdUsuarioNavigation);
+            return View(await clinicaReumatologiaContext.ToListAsync());
+        }
+
+        // GET: Pagos/Details/5
+        public async Task<IActionResult> Detalles(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pago = await _context.Pagos
+                .Include(p => p.IdUsuarioNavigation)
+                .FirstOrDefaultAsync(m => m.IdPago == id);
+            if (pago == null)
+            {
+                return NotFound();
+            }
+
+            return View(pago);
+        }
+
+        // GET: Pagos/Create
+        public IActionResult Crear()
+        {
+            ViewData["IdUsuario"] = new SelectList(_context.Usuarios, "IdUsuario", "IdUsuario");
+            return View();
+        }
+
+        // POST: Pagos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Crear([Bind("IdPago,IdUsuario,TotalPagar,Detalle")] Pago pago)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(pago);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdUsuario"] = new SelectList(_context.Usuarios, "IdUsuario", "IdUsuario", pago.IdUsuario);
+            return View(pago);
+        }
+
+        // GET: Pagos/Edit/5
+        public async Task<IActionResult> Editar(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pago = await _context.Pagos.FindAsync(id);
+            if (pago == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdUsuario"] = new SelectList(_context.Usuarios, "IdUsuario", "IdUsuario", pago.IdUsuario);
+            return View(pago);
+        }
+
+        // POST: Pagos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Editar(int id, [Bind("IdPago,IdUsuario,TotalPagar,Detalle")] Pago pago)
+        {
+            if (id != pago.IdPago)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(pago);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PagoExists(pago.IdPago))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdUsuario"] = new SelectList(_context.Usuarios, "IdUsuario", "IdUsuario", pago.IdUsuario);
+            return View(pago);
+        }
+
+        // GET: Pagos/Delete/5
+        public async Task<IActionResult> Eliminar(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pago = await _context.Pagos
+                .Include(p => p.IdUsuarioNavigation)
+                .FirstOrDefaultAsync(m => m.IdPago == id);
+            if (pago == null)
+            {
+                return NotFound();
+            }
+
+            return View(pago);
+        }
+
+        // POST: Pagos/Delete/5
+        [HttpPost, ActionName("Eliminar")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var pago = await _context.Pagos.FindAsync(id);
+            if (pago != null)
+            {
+                _context.Pagos.Remove(pago);
+                await _context.SaveChangesAsync();
+                return Json(new { success = true });
+            }
+
+            return Json(new { success = false });
+
+            //await _context.SaveChangesAsync();
+            //return RedirectToAction(nameof(Index));
+        }
+
+        private bool PagoExists(int id)
+        {
+            return _context.Pagos.Any(e => e.IdPago == id);
+        }
+    }
+}
--- Proyecto_Final_CentroMedico/obj/Debug/net8.0/Proyecto_Final_CentroMedico.AssemblyInfo.cs
+++ Proyecto_Final_CentroMedico/obj/Debug/net8.0/Proyecto_Final_CentroMedico.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto_Final_CentroMedico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59adc0d148a535b3b32018511bd346a467ba8831")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9635a4867fcd10e17e42113a2128c7db028d6c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto_Final_CentroMedico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto_Final_CentroMedico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
